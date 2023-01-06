4f521c66008c55efc96b7f831e4dc050f24675bf
watamase <yazewatamase@gmail.com>
2023-01-07 01:15:18 +0700

    CheatBot

---
+++ CheatBot/__init__.py
@@ -0,0 +1,11 @@
+from pyrogram import Client
+
+api_id = None  # int/str (https://my.telegram.org/app)
+api_hash = None  # str (https://my.telegram.org/app)
+
+CheatBot = Client(
+    name="CheatBot",
+    api_id=api_id,
+    api_hash=api_hash,
+)
+
---
+++ CheatBot/__main__.py
@@ -0,0 +1,53 @@
+from CheatBot import CheatBot
+from datetime import datetime
+
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+from pyrogram.errors.exceptions.bad_request_400 import UserBot
+from pyrogram.errors.exceptions.forbidden_403 import UserPrivacyRestricted
+
+RED = "\u001b[31;1m"
+GREEN = "\u001b[32;1m"
+YELLOW = "\u001b[33;1m"
+RESET = "\u001b[0m"
+
+CHAT_ID = None  # int
+CHANNEL_ID = None  # int
+
+
+async def main():
+    async with CheatBot:
+        async for member in CheatBot.get_chat_members(CHAT_ID):
+            ctime = datetime.now().strftime("%H:%M:%S")
+
+            print(
+                GREEN + ctime,
+                RESET + f"{member.user.id} ({member.user.first_name})",
+            )
+
+            try:
+                await CheatBot.add_chat_members(
+                    CHANNEL_ID,
+                    member.user.id,
+                )
+            except UserBot:
+                print(
+                    RED + ctime,
+                    RESET + f"{member.user.id}",
+                    YELLOW + "(User bot)",
+                )
+                continue
+            except UserPrivacyRestricted:
+                print(
+                    RED + ctime,
+                    RESET + f"{member.user.id}",
+                    YELLOW + "(Privacy restricted)",
+                )
+                continue
+
+
+if __name__ == "__main__":
+    try:
+        CheatBot.run(main())
+    except KeyboardInterrupt:
+        exit()
+

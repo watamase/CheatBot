8636b1f8b801690b668e2ba6c38d3ffbbfde2308
Виктор <yazewatamase@gmail.com>
2023-01-09 01:47:10 +0700

    CheatBot

--- CheatBot/__main__.py
+++ CheatBot/__main__.py
@@ -1,8 +1,13 @@
+import asyncio
+
 from CheatBot import CheatBot
 from datetime import datetime
 
-from pyrogram.errors.exceptions.bad_request_400 import UserBot
+from pyrogram.errors.exceptions.flood_420 import FloodWait
 from pyrogram.errors.exceptions.forbidden_403 import UserPrivacyRestricted
+from pyrogram.errors.exceptions.bad_request_400 import UserNotMutualContact
+from pyrogram.errors.exceptions.bad_request_400 import UserBot
+from pyrogram.errors.exceptions.bad_request_400 import PeerFlood
 
 RED = "\u001b[31;1m"
 GREEN = "\u001b[32;1m"
@@ -10,48 +15,44 @@ YELLOW = "\u001b[33;1m"
 RESET = "\u001b[0m"
 
 
-async def user_invitation(
-    chat_id: int,
-    channel_id: int,
-):
-    async with CheatBot:
-        async for member in CheatBot.get_chat_members(chat_id):
-            ctime = datetime.now().strftime("%H:%M:%S")
+async def user_invitation(parse_from: int = None, invite_to: int = None) -> None:
 
-            print(
-                GREEN + ctime,
-                RESET + f"{member.user.id} ({member.user.first_name})",
-            )
+    if parse_from is None and invite_to is None:
+        print(RED + datetime.now().strftime("%H:%M:%S"), RESET + "Pass values to function arguments")
+        return
+
+    async with CheatBot:
+        async for member in CheatBot.get_chat_members(parse_from):
+            time = datetime.now().strftime("%H:%M:%S")
 
             try:
-                await CheatBot.add_chat_members(
-                    channel_id,
-                    member.user.id,
-                )
-            except UserBot:
-                print(
-                    RED + ctime,
-                    RESET + f"{member.user.id}",
-                    YELLOW + "(User bot)",
-                )
-                continue
-            except UserPrivacyRestricted:
-                print(
-                    RED + ctime,
-                    RESET + f"{member.user.id}",
-                    YELLOW + "(Privacy restricted)",
-                )
+                await CheatBot.add_chat_members(invite_to, member.user.id)
+                print(GREEN + time, RESET + f"{member.user.id} ({member.user.first_name})")
                 continue
 
+            except FloodWait as e:
+                print(RED + time, RESET + "Flood wait:", YELLOW + f"{e.value}")
+                await asyncio.sleep(e.value)
+                continue
+
+            except UserBot:
+                print(RED + time, RESET + f"{member.user.id}", YELLOW + "(User bot)")
+                continue
+
+            except UserPrivacyRestricted:
+                print(RED + time, RESET + f"{member.user.id}", YELLOW + "(Privacy restricted)")
+                continue
+
+            except UserNotMutualContact:
+                print(RED + time, RESET + f"{member.user.id}", YELLOW + "(Not mutual contact)")
+
+            except PeerFlood:
+                print(RED + time, RESET + "Peer flood", YELLOW + "(Account limited)")
+                break
+        return
 
 if __name__ == "__main__":
     try:
-        CheatBot.run(
-            user_invitation(
-                None,
-                None,
-            )
-        )
+        CheatBot.run(user_invitation(None, None))  # Replace with chat/channel IDs
     except KeyboardInterrupt:
         exit()
-
